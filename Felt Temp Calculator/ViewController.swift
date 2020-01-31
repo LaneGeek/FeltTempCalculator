ee2bdd262699e950295280b901dc1ceec4cbea76
HS <sekhah@my.lanecc.edu>
2020-01-31 08:02:28 -0800

    Initial Commit

---
+++ Felt Temp Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Felt Temp Calculator
+//
+//  Created by Wolfgang on 1/31/20.
+//  Copyright © 2020 Wolfgang's Software Factory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
