2123000acf3d95c5c29cb59f6661d300e94ba897
HS <sekhah@my.lanecc.edu>
2020-02-14 12:33:22 -0800

    Added comments to History View Controller.

--- Felt Temp Calculator/HistoryTableViewController.swift
+++ Felt Temp Calculator/HistoryTableViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 class HistoryTableViewController: UITableViewController {
     
+    // The field which stores the history
     var history: [String] = []
 
     override func viewDidLoad() {
@@ -9,11 +10,14 @@ class HistoryTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // The number of rows
         return history.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Temporary cell to filled with data
         let cell = UITableViewCell()
+        // Fill it with a reading from history
         cell.textLabel?.text = history[indexPath.row]
         return cell
     }
